5135da164c767f10cc57cc6a463be91a9750ceea
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2023-02-22 20:50:18 -0300

    Feat: Criando alguns exemplos de variaveis

--- 3-Conhecendo-os-tipos-de-C#/variaveis/Program.cs
+++ 3-Conhecendo-os-tipos-de-C#/variaveis/Program.cs
@@ -2,3 +2,24 @@
 int x;
 decimal y;
 bool z;
+
+// Tipo string
+string fraseDeUmLivro = "Ser ou não ser, eis a questão";
+
+// Tipo Float
+float valorFloat = 100.0f; //obrigatório o uso do 'f' no final
+
+// Tipo double
+double valorDouble = 10.0d; 
+
+// Tipo decimal
+decimal valorDecimal = 100.0M;
+
+// Tipo char
+var letra = 'T';
+
+// Tipo implicito como string
+var curso = "Trybe - Aceleração c#";
+
+// tipo implicito como decimal
+var media = 6.53M;
