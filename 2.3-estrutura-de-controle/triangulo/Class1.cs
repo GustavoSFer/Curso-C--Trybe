612c9b4efc5a8d0f962939ff29c69f02ed210a77
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2023-02-28 21:25:21 -0300

    Feat: Criando a função que retorna o tipo de triangulo

--- 2.3-estrutura-de-controle/triangulo/Class1.cs
+++ 2.3-estrutura-de-controle/triangulo/Class1.cs
@@ -1,5 +1,17 @@
 ﻿namespace triangulo;
 public class Class1
 {
+  public static string IdentifyTriangule(double xSide, double ySide, double zSide) {
+    var name = "";
+
+    if(xSide == ySide && xSide == zSide) {
+      name = "Triangulo Equilátero";
+    } else if((xSide == ySide) || (xSide == zSide) || (ySide ==zSide)){
+      name = "Triângulo Isóscele";
+    } else {
+        name = "Triângulo Escaleno";
+    }
+    return name;
+  }
 
 }
