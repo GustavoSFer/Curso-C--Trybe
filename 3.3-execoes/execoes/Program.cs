3c91312a52d7d5d7d6d478f6fd2cccfb9f999514
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2023-03-09 20:57:45 -0300

    Feat: Realizando uma função que realiza a divisão

--- 3.3-execoes/execoes/Program.cs
+++ 3.3-execoes/execoes/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Program;
+class TestClass
+{
+  static void Main(string[] args)
+  {
+    var classe = new TestClass();
+    Console.WriteLine(classe.DivideNumbers(12, 4));
+  }
+  public int DivideNumbers(int numberOne, int numberTwo)
+  {
+    return numberOne / numberTwo;
+  }
+}
