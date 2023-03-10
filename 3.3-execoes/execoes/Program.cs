328c5a97179cb8b809ff06c43a2739d525876074
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2023-03-09 21:17:52 -0300

    Fix: Arrumando a nossa função que retorna um throw

--- 3.3-execoes/execoes/Program.cs
+++ 3.3-execoes/execoes/Program.cs
@@ -1,17 +1,19 @@
 ﻿namespace Program;
-class TestClass
+class Program
 {
   static void Main(string[] args)
   {
-    var classe = new TestClass();
+    var classe = new Program();
     Console.WriteLine(classe.DivideNumbers(10, 0));
   }
   public int DivideNumbers(int numberOne, int numberTwo)
   {
     try {
+      if (numberTwo == 0) throw new DivideByZeroException("O numero não pode ser dividido por 0");
       return numberOne / numberTwo;
     } catch (DivideByZeroException ex) {
-      throw new DivideByZeroException("O numero não pode ser dividido por 0");
+      Console.WriteLine(ex.Message);
+      throw;
     }
   }
 }
