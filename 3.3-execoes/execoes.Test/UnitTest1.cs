d8a4195c2e45e4f818ad8679ba8f67bb0d03626e
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2023-03-09 21:31:22 -0300

    Test: Realizando teste do sistema

--- 3.3-execoes/execoes.Test/UnitTest1.cs
+++ 3.3-execoes/execoes.Test/UnitTest1.cs
@@ -1,12 +1,21 @@
+using System;
 using Xunit;
+using FluentAssertions;
 
 namespace execoes.Test;
 
 public class UnitTest1
 {
-    [Fact]
-    public void Test1()
+    [Theory(DisplayName = "Teste para lançamento de exceções em divisão entre dois numeros")]
+    [InlineData(2, 0)]
+    [InlineData(12, 0)]
+    [InlineData(23, 0)]
+    [InlineData(343, 0)]
+    public void TestDivideNumbersException(int entry1, int entry2)
     {
+        var classe = new Program();
+        Action act = () => classe.DivideNumbers(entry1, entry2);
 
+        act.Should().Throw<DivideByZeroException>().WithMessage("O numero não pode ser dividido por 0");
     }
 }
