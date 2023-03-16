7e01c831658aa76c62173f2fa8a5191791ca838c
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2023-03-15 21:33:56 -0300

    Feat: construtor

--- 4.1-Abstracao-classe-em-c-sharp/rocket/Main.cs
+++ 4.1-Abstracao-classe-em-c-sharp/rocket/Main.cs
@@ -2,11 +2,11 @@ namespace rocket;
 
 public class Program {
   public void Main(String[] args ) {
-    var rocket1 = new Rocket();
-    var rocket2 = new Rocket();
+    var rocket1 = new Rocket("Apollo 11");
+    var rocket2 = new Rocket("Falcom 9");
 
-    rocket1.Name = "Apollo 11";
-    rocket2.Name = "Falcom 9";
+    // rocket1.Name = "Apollo 11";
+    // rocket2.Name = "Falcom 9";
 
     Console.WriteLine(rocket1.Name);
   }
