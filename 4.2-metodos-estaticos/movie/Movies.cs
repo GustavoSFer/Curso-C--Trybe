d6f46f21e8a515ab3fb612b8316deccc433720ff
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2023-03-16 21:17:32 -0300

    Criando uma classe filme

---
+++ 4.2-metodos-estaticos/movie/Movies.cs
@@ -0,0 +1,9 @@
+class Movie {
+  public string Title { get; set; }
+  public string Category { get; set; }
+
+  public Movie (string title, string category) {
+    Title = title;
+    Category = category;
+  }
+}
