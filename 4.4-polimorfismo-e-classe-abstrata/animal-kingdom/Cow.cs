911b9dfb9c8a9d11589cbdf8fad5af45b0fbaf0f
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2023-03-23 20:59:32 -0300

    Criando a classe wolf e cow

---
+++ 4.4-polimorfismo-e-classe-abstrata/animal-kingdom/Cow.cs
@@ -0,0 +1,9 @@
+public class Cow {
+  public string ScientificName = "Bos taurus";
+  public bool CanEatMeat = false;
+  public bool CanEatVegetables = true;
+
+  public void Eat() {
+    Console.WriteLine("The cow has eaten some vegetables!");
+  }
+}
---
+++ 4.4-polimorfismo-e-classe-abstrata/animal-kingdom/Wolf.cs
@@ -0,0 +1,11 @@
+public class Wolf
+{
+  public string ScientificName = "Canis lupus";
+  public bool CanEatMeat = true;
+  public bool CanEatVegetables = false;
+
+  public void Eat()
+  {
+    Console.WriteLine("The wolf has eaten some meat");
+  }
+}
