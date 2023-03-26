2c841fedccb88c3e08bcd05c91d89b2e454b6364
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2023-03-26 17:22:49 -0300

    Feat: Criando nossas classes com o modificador public

--- Modificador-de-acesso/mod-de-acesso/Program.cs
+++ Modificador-de-acesso/mod-de-acesso/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Point
+{
+  public int x;
+  public int y;
+
+  public void PrintPoint(){
+    Console.WriteLine($"(X - Y) - ({x} - {y})");
+  }
+}
+
+
+public class Program
+{
+  public static void Main(String[] args)
+  {
+    var ponto = new Point();
+    ponto.x = 2;
+    ponto.y = 10;
+
+    ponto.PrintPoint();
+  }
+}
