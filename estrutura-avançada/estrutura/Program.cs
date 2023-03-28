cfa3b2496a1aabff021bebde56f84f8f36053c3a
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2023-03-27 21:59:14 -0300

    Feat: Criando uma Enum

--- estrutura-avançada/estrutura/Program.cs
+++ estrutura-avançada/estrutura/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public enum PaymentType
+{
+  credit,
+  debit
+}
+
+public static class Program
+{
+  public static void Main()
+  {
+
+  }
+}
