dbacff7d712f9e40d3ae89b4ef9d6456e7ce3ab3
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2023-03-27 22:06:57 -0300

    Feat: Verificando os dados sende impresso

--- estrutura-avançada/estrutura/Program.cs
+++ estrutura-avançada/estrutura/Program.cs
@@ -1,13 +1,18 @@
 ﻿public enum PaymentType
 {
   credit,
-  debit
+  debit,
+  InCash = debit,
+  InParts = credit,
 }
 
 public static class Program
 {
   public static void Main()
   {
-
+    Console.WriteLine("String Crédito " + PaymentType.credit + " Valor Crédito: " + (int)PaymentType.credit );
+    Console.WriteLine("String Debito " + PaymentType.debit + " Valor Debito: " + (int)PaymentType.debit );
+    Console.WriteLine("String InCash " + PaymentType.InCash + " Valor InCash: " + (int)PaymentType.InCash );
+    Console.WriteLine("String InParts " + PaymentType.InParts + " Valor InParts: " + (int)PaymentType.InParts );
   }
 }
