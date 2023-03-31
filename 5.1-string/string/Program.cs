9887bb8dfbbafbd69bb4e07d60cb3f18f265f2eb
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2023-03-31 15:43:51 -0300

    Entendendo o concat da string

--- 5.1-string/string/Program.cs
+++ 5.1-string/string/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Program
+{
+  public static void Main(String[] args)
+  {
+    var concatenandoString = new Concat("Olá ", "Mundo!!");
+    Console.WriteLine(concatenandoString.Concatenando());
+
+  }
+
+  // Contatenação de string
+  public class Concat {
+    string TextOne;
+    string TextTwo;
+    public Concat(string textOne, string textTwo) {
+      TextOne = textOne;
+      TextTwo = textTwo;
+    }
+    public string Concatenando() {
+      string result = string.Concat(TextOne, TextTwo);
+      return result;
+    }
+  }
+}
