a109831ec6061a16a66af65ee0ae3d582c2fa1d1
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2023-04-01 17:39:43 -0300

    Feat: Verificando que dia será com o TimeSpan

--- 5.2-datas/datas/DataUtil.cs
+++ 5.2-datas/datas/DataUtil.cs
@@ -7,15 +7,31 @@
     //     }
     // }
 
+/* VERIFICANDO QUE DIA SERÁ APOS UM N DIAS INFORMADO EM ADD DAYS(DIAS)*/
+    // public class DataUtil
+    // {
+    //     public static void Main(string[] args)
+    //     {
+    //         var today = DateTime.Now;
+    //         var answer = today.AddDays(36);
 
-    public class DataUtil
+    //         System.Console.WriteLine("Hoje é " +today.Day +"/" +today.Month +" - " +today.DayOfWeek);
+    //         System.Console.WriteLine("Daqui a 36 dias será "+answer.Day +"/" +answer.Month +" - " +answer.DayOfWeek);
+    //     }
+    // }
+
+
+
+        public class DataUtil
     {
         public static void Main(string[] args)
         {
-            var today = DateTime.Now;
-            var answer = today.AddDays(36);
+            var today = System.DateTime.Now;
+            var duration = new System.TimeSpan(-36, 0, 0, 0);
+            var answer = today.Add(duration);
 
             System.Console.WriteLine("Hoje é " +today.Day +"/" +today.Month +" - " +today.DayOfWeek);
-            System.Console.WriteLine("Daqui a 36 dias será "+answer.Day +"/" +answer.Month +" - " +answer.DayOfWeek);
+            System.Console.WriteLine("36 atrás era "+answer.Day +"/" +answer.Month +" - " +answer.DayOfWeek);
         }
     }
+    
