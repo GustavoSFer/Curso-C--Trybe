4c4879399f00b9ad5d90186580e52cd9d01ba717
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2023-04-01 16:56:03 -0300

    Feat: vendo  a data e o tipo

---
+++ 5.2-datas/datas/DataUtil.cs
@@ -0,0 +1,8 @@
+    public class DataUtil
+    {
+        public static void Main(string[] args)
+        {
+            var dataType = DateTime.Now;
+            Console.WriteLine(dataType.GetType());
+        }
+    }
