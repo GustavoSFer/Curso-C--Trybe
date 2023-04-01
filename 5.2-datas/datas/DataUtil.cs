e7fa7c0349bbec2672b53dbae4e3ff64e54eb46e
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2023-04-01 17:38:04 -0300

    Feat: Mostrando que dia sera daqui a x dias com o metodo AddDays

--- 5.2-datas/datas/DataUtil.cs
+++ 5.2-datas/datas/DataUtil.cs
@@ -1,8 +1,21 @@
+    // public class DataUtil
+    // {
+    //     public static void Main(string[] args)
+    //     {
+    //         var dataType = DateTime.Now;
+    //         Console.WriteLine(dataType.GetType());
+    //     }
+    // }
+
+
     public class DataUtil
     {
         public static void Main(string[] args)
         {
-            var dataType = DateTime.Now;
-            Console.WriteLine(dataType.GetType());
+            var today = DateTime.Now;
+            var answer = today.AddDays(36);
+
+            System.Console.WriteLine("Hoje é " +today.Day +"/" +today.Month +" - " +today.DayOfWeek);
+            System.Console.WriteLine("Daqui a 36 dias será "+answer.Day +"/" +answer.Month +" - " +answer.DayOfWeek);
         }
     }
