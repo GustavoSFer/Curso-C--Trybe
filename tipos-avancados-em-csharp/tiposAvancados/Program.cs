9ec4362af9b56a37e2b37a683708239670f377f7
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2023-04-04 21:58:59 -0300

    Feat: Criando o programa para adicionar

--- tipos-avancados-em-csharp/tiposAvancados/Program.cs
+++ tipos-avancados-em-csharp/tiposAvancados/Program.cs
@@ -1,38 +1,50 @@
 ﻿public class GenericList<T>
 {
-    private class Node
+  private class Node
+  {
+    public T Value;
+    public Node? Next;
+
+    public Node(T t)
     {
-        public T Value;
-        public Node? Next;
-
-        public Node(T t)
-        {
-            Value = t;
-            Next = null;
-        }
+      Value = t;
+      Next = null;
     }
+  }
 
-    private Node Head;
+  private Node Head;
 
-    public GenericList()
+  public GenericList()
+  {
+    Head = null;
+  }
+
+  public void Add(T input)
+  {
+    if (Head == null)
     {
-        Head = null;
+      Head = new Node(input);
+      Console.WriteLine("Nó Head criado!");
     }
-
-     public void Add(T input) 
+    else
     {
-        if (Head == null)
-        {
-            Head = new Node(input);
-            Console.WriteLine("Nó Head criado!");
-        }
-        else
-        {
-            //Encontra onde inserir o próximo nó na lista.
-            Node lastNode = Head;
-            while(lastNode.Next != null)   lastNode = lastNode.Next;
+      //Encontra onde inserir o próximo nó na lista.
+      Node lastNode = Head;
+      while (lastNode.Next != null) lastNode = lastNode.Next;
 
-            lastNode.Next = new Node(input);                        
-        }
+      lastNode.Next = new Node(input);
     }
+  }
+}
+
+
+public class Program
+{
+  public static void Main(String[] args)
+  {
+    GenericList<int> list1 = new GenericList<int>();
+    list1.Add(1);
+    list1.Add(12);
+    list1.Add(4);
+  }
 }
