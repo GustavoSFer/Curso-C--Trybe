a77263eeafa8bcbea83d9759e918e5de39505028
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2023-04-04 21:51:06 -0300

    Feat: Criando uma classe tipo generico de lista

--- tipos-avancados-em-csharp/tiposAvancados/Program.cs
+++ tipos-avancados-em-csharp/tiposAvancados/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class GenericList<T>
+{
+    private class Node
+    {
+        public T Value;
+        public Node? Next;
+
+        public Node(T t)
+        {
+            Value = t;
+            Next = null;
+        }
+    }
+
+    private Node Head;
+
+    public GenericList()
+    {
+        Head = null;
+    }
+
+     public void Add(T input) 
+    {
+        if (Head == null)
+        {
+            Head = new Node(input);
+            Console.WriteLine("Nó Head criado!");
+        }
+        else
+        {
+            //Encontra onde inserir o próximo nó na lista.
+            Node lastNode = Head;
+            while(lastNode.Next != null)   lastNode = lastNode.Next;
+
+            lastNode.Next = new Node(input);                        
+        }
+    }
+}
