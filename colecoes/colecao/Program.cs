cf7b577d0c85358a36b87397c442df9c73414a49
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2023-04-05 21:45:04 -0300

    Feat: Criando uma lista e imprimindo ela

--- colecoes/colecao/Program.cs
+++ colecoes/colecao/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Program {
+  public static void Main(string[] args) {
+    List<string> nomes = new List<string>(){ "Carro", "Casa", "Roupas", "Avião" };
+
+    // Imprimindo a nossa lista
+    foreach (var item in nomes) {
+      Console.WriteLine(item);
+    }
+  }
+}
