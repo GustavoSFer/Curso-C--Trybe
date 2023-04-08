3e7baf6e1c0132665f75e299e6ea5df43f6a60de
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2023-04-08 15:24:39 -0300

    Arrumando  lint

--- colecoes/colecao/Program.cs
+++ colecoes/colecao/Program.cs
@@ -1,9 +1,12 @@
-﻿public class Program {
-  public static void Main(string[] args) {
-    List<string> nomes = new List<string>(){ "Carro", "Casa", "Roupas", "Avião" };
+﻿public class Program
+{
+  public static void Main(string[] args)
+  {
+    List<string> nomes = new List<string>() { "Carro", "Casa", "Roupas", "Avião" };
 
     // Imprimindo a nossa lista
-    foreach (var item in nomes) {
+    foreach (var item in nomes)
+    {
       Console.WriteLine(item);
     }
 
@@ -11,7 +14,8 @@
     // Adicionando novo item a nossa lista e imprimindo
     nomes.Add("Celular");
 
-    foreach (var item in nomes) {
+    foreach (var item in nomes)
+    {
       Console.WriteLine(item);
     }
 
@@ -19,34 +23,37 @@
     // Removendo o item "Casa" da nossa lista
     nomes.Remove("Casa");
 
-    foreach (var item in nomes) {
+    foreach (var item in nomes)
+    {
       Console.WriteLine(item);
     }
 
-     Console.WriteLine("**************************");
+    Console.WriteLine("**************************");
     // Removendo o item pelo index da nossa lista
     nomes.RemoveAt(2);
 
-    foreach (var item in nomes) {
+    foreach (var item in nomes)
+    {
       Console.WriteLine(item);
     }
-   
-      Console.WriteLine("**************************");
+
+    Console.WriteLine("**************************");
     // procurando elemento na nossa lista e retorna o index
     int index = nomes.IndexOf("Roupas");
-      Console.WriteLine(index);
-   
+    Console.WriteLine(index);
 
-   Console.WriteLine("**************************");
-   // Realizando a ordenação dos elementos
-   // Primeiro vamos adicionar alguns elementos novamente
-   nomes.Add("Pessoas");
-   nomes.Add("Frutas");
-   nomes.Add("Xadrez");
-   nomes.Add("Lojas");
+
+    Console.WriteLine("**************************");
+    // Realizando a ordenação dos elementos
+    // Primeiro vamos adicionar alguns elementos novamente
+    nomes.Add("Pessoas");
+    nomes.Add("Frutas");
+    nomes.Add("Xadrez");
+    nomes.Add("Lojas");
 
     nomes.Sort();
-    foreach (var item in nomes) {
+    foreach (var item in nomes)
+    {
       Console.WriteLine(item);
     }
   }
