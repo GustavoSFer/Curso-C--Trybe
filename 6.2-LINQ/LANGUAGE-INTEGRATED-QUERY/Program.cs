72dc1eb029956cff5a310008d2ba9f0ecead20b8
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2023-04-08 18:02:18 -0300

    Feat: Criando o mesmo filtro com o formato lambda

--- 6.2-LINQ/LANGUAGE-INTEGRATED-QUERY/Program.cs
+++ 6.2-LINQ/LANGUAGE-INTEGRATED-QUERY/Program.cs
@@ -2,15 +2,29 @@
 {
   public static void Main(string[] args)
   {
-    declarativeLinq();
+    // declarativeLinq();
+    lambdaLinq();
   }
 
-  static void declarativeLinq() {
+  static void declarativeLinq()
+  {
 
-    string[] games = {"Fortnite", "Valorant", "Destiny", "Call of Duty", "World of Warcraft"};
+    string[] games = { "Fortnite", "Valorant", "Destiny", "Call of Duty", "World of Warcraft" };
     var filterGames = from game in games
                       where game.Contains('a')
                       select game;
+
+    foreach (string game in filterGames)
+    {
+      Console.WriteLine(game);
+    }
+  }
+
+  static void lambdaLinq()
+  {
+    string[] games = { "Fortnite", "Valorant", "Destiny", "Call of Duty", "World of Warcraft" };
+
+    var filterGames = games.Where(n => n.Contains('a'));
 
     foreach (string game in filterGames) {
       Console.WriteLine(game);
