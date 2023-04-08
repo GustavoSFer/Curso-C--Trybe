c7cd763197679d71f71c013dfa8452b4efd85968
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2023-04-08 17:59:09 -0300

    Feat: Criando um metodo que filtra os jogos que contem a letra a no nome

--- 6.2-LINQ/LANGUAGE-INTEGRATED-QUERY/Program.cs
+++ 6.2-LINQ/LANGUAGE-INTEGRATED-QUERY/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Program
+{
+  public static void Main(string[] args)
+  {
+    declarativeLinq();
+  }
+
+  static void declarativeLinq() {
+
+    string[] games = {"Fortnite", "Valorant", "Destiny", "Call of Duty", "World of Warcraft"};
+    var filterGames = from game in games
+                      where game.Contains('a')
+                      select game;
+
+    foreach (string game in filterGames) {
+      Console.WriteLine(game);
+    }
+  }
+}
