a6ad3ec5cb1b7c3fb0b0860cd87dbf9b4d2282dc
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2023-04-11 20:59:21 -0300

    criando alguns dados

--- 6.3-linq-avancado/linqAvancado/Program.cs
+++ 6.3-linq-avancado/linqAvancado/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class State
+{
+    public string Name { get; set; } = "";
+    public string Abbreviation { get; set; } = "";
+    public string Region { get; set; } = "";
+}
+
+class State
+{
+    public string Name { get; set; } = "";
+    public string Abbreviation { get; set; } = "";
+    public string Region { get; set; } = "";
+}
+
+State[] states = {
+    new State { Name="Acre", Abbreviation="AC", Region="Norte" },
+    new State { Name="Alagoas", Abbreviation="AL", Region="Nordeste" },
+    // [...]
+    new State { Name="Pará", Abbreviation="PA", Region="Norte" },
+    new State { Name="Paraíba", Abbreviation="PB", Region="Nordeste" },
+    // [...]
+};
+
+City[] cities = {
+    new City { Name="Belém", StateAbbreviation="PA" },
+    new City { Name="Rio Branco", StateAbbreviation="AC" },
+    new City { Name="Maceió", StateAbbreviation="AL" },
+    // [...]
+};
