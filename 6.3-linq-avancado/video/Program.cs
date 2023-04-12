0d6c21c1a5ffe86ab4ae26ef4eb443e2937b0104
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2023-04-11 21:08:01 -0300

    Feat: Criando uma classe pet, colocando valores e verificando a soma o maximo e a media das idades

---
+++ 6.3-linq-avancado/video/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+  static void Main(string[] args)
+  {
+    Pet[] pets = {
+      new Pet { Name="rex", Age=8 },
+      new Pet { Name="Tóto", Age=3 },
+      new Pet { Name="Slinky", Age=2 },
+      new Pet { Name="Belinha", Age=1 },
+    };
+
+    var sumAge = pets.Sum(pet => pet.Age);
+    var maxAge = pets.Max(pet => pet.Age);
+    Console.WriteLine("O Pet mais velho tem " + maxAge + " anos");
+
+    var averageAge = pets.Average(pet => pet.Age);
+    Console.WriteLine("A idade média dos cachorro é: " + averageAge);
+  }
+}
