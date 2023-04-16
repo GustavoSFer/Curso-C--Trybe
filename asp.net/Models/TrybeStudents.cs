7b303a9b11526cbaa6d0c90b9b3b67af8f77f58e
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2023-04-16 10:18:55 -0300

    Criando nossa model

---
+++ asp.net/Models/TrybeStudents.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace TrybeApp.Models
+{
+    public class TrybeStudent
+    {
+      public int Id { get; set; }
+    	public string Name { get; set;  }
+    	public int Age { get; set;  }
+    	public int ClassNumber { get; set;  }
+    }
+}
